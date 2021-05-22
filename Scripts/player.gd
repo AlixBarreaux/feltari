35178966b6921517c111b6eca790c47b3cdcf4ed
alixbarreaux <alixbarreaux@tutanota.com>
2021-05-22 21:04:19 +0200

    Add audio buses for music and sound effects, player and enemy damage with hurtboxes and remove unused files at the root of the project hierarchy

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,7 +12,14 @@ export var current_speed: int = 200 setget set_current_speed, get_current_speed
 var direction: Vector2 = Vector2(0.0, 0.0) setget set_direction, get_direction
 var velocity: Vector2 = Vector2(0.0, 0.0) setget set_velocity
 
+var can_move: bool = false
+var can_melee_attack = true
+
 ################################# RUN THE CODE #################################
+
+func _ready() -> void:
+	self._initialize_asserts()
+
 
 func _physics_process(_delta: float) -> void:
 	self.calculate_velocity()
@@ -41,6 +48,14 @@ func _unhandled_input(_event: InputEvent) -> void:
 
 ############################### DECLARE FUNCTIONS ##############################
 
+func _initialize_asserts() -> void:
+	# Must be more than 0!
+	assert(self.current_health > 0)
+	assert(self.max_health > 0)
+	# Current Health mus be less or equal to max health!
+	assert(self.current_health <= self.max_health)
+
+
 func set_direction(new_direction: Vector2) -> void:
 	direction = new_direction
 
@@ -63,3 +78,38 @@ func get_current_speed() -> int:
 
 func calculate_velocity() -> void:
 	self.velocity = self.direction * self.current_speed
+
+
+func melee_attack_animation_finished() -> void:
+	can_melee_attack = true
+
+
+
+
+export var current_health: int = 0
+export var max_health: int = 0
+export var damage: int = 1
+
+func take_damage(amount: int) -> void:
+	self.current_health -= amount
+	print(self.name + ": I took " + str(amount) + " damage!")
+	
+	self.check_if_dead()
+	
+	
+
+func check_if_dead() -> void:
+	if self.current_health <= 0:
+			self.current_health = 0
+			self.die()
+	print(self.name + ": Health status: " + str(current_health) + " / " + str(max_health))
+
+
+func die() -> void:
+	print(self.name + str(": I died!"))
+
+
+func _on_HurtBox_body_entered(body: PhysicsBody2D) -> void:
+	print(self.name, ": I was entered by: ", body.name)
+	body.take_damage(self.damage)
+	
