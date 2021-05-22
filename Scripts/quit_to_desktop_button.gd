c12341003037126f421900090fb559c5444f2718
alixbarreaux <alixbarreaux@tutanota.com>
2021-05-22 18:25:08 +0200

    Add Main Menu with translations and ability to quit the game to desktop and load the World scene

---
+++ Scripts/quit_to_desktop_button.gd
@@ -0,0 +1,15 @@
+class_name QuitToDesktopButton
+extends Button
+
+
+############################### DECLARE VARIABLES ##############################
+
+
+################################# RUN THE CODE #################################
+
+
+############################### DECLARE FUNCTIONS ##############################
+
+
+func _on_QuitToDesktopButton_pressed() -> void:
+	get_tree().quit()
